408c475d9d46fafeb3818ba532b9adda4fbe9ff4
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-11-09 00:55:36 +0100

    Projects

---
+++ src/utils/projects.ts
@@ -0,0 +1,25 @@
+
+
+export const projects = [
+	{
+		id: 1,
+		title: "Językowy Punkt Widzenia",
+		link: "https://jpw-kursy.pl/",
+		img: "/jpw.png",
+		desc: "Szkoła języków obcych online + wdrożenie platformy e-learningowej w systemie Moodle",
+	},
+	{
+		id: 2,
+		title: "Parafia w Parowej",
+		link: "https://parafiaparowa.pl/",
+		img: "/parowa.png",
+		desc: "Strona internetowa dla parafii rzymskokatolickiej",
+	},
+	{
+		id: 3,
+		title: "Monthly Modifiers",
+		link: "https://monthlymodifiers.com/",
+		img: "/modifiers.png",
+		desc: "Aplikacja webowa do self-improvementu",
+	},
+];
