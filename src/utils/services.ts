b5ac766dbd784e3df6ce60a9b40f98fed0854056
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-11-08 21:07:06 +0100

    Services section

---
+++ src/utils/services.ts
@@ -0,0 +1,23 @@
+export const services = [
+	{
+		id: 1,
+		icon: "/devices.svg",
+		alt: "ikona telefonu i komputera",
+		title: "Responsywne strony internetowe",
+		desc: "Tworzymy nowoczesne projekty dostosowane do Twoich potrzeb. Dostępne na wszystkich urządzeniach",
+	},
+	{
+		id: 2,
+		icon: "/brush.svg",
+		alt: "ikona pędzla",
+		title: "Spersonalizowany design",
+		desc: "Projekt dostosowany do Twoich potrzeb i estetyki",
+	},
+	{
+		id: 3,
+		icon: "/sos.svg",
+		alt: "napis 'SOS'",
+		title: "Wsparcie techniczne",
+		desc: "Po zakończeniu współpracy udzielimy Ci pomocy w przypadku jakichkolwiek problemów. Nie zostaniesz sam/a",
+	},
+];
