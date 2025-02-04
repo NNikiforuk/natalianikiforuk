9a26a1f6fabfcd3595bc99f5291d8309d0a9ad63
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-02-04 14:20:08 +0100

    GTM configuration try

--- src/scripts/gtm.js
+++
@@ -1,9 +0,0 @@
-export const initGTM = () => {
-	window.dataLayer = window.dataLayer || [];
-	window.dataLayer.push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-
-	const script = document.createElement("script");
-	script.async = true;
-	script.src = `https://www.googletagmanager.com/gtm.js?id=GTM-W5B6WX2M`;
-	document.head.appendChild(script);
-};
